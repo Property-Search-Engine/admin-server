9ba1b20ad885d9f94becbb653dc3241363a5df96
devmxhv <mahmoud.mostafa3107@gmail.com>
2021-01-29 11:45:05 +0100

    Added firebase configuration and defined AuthMiddleware

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const { json } = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
+const { authMiddleware } = require("./middleware/auth-middleware")
 
 require("dotenv").config();
 
@@ -12,7 +13,7 @@ const errorMiddleware = require("./middleware/error-middleware");
 const userRouter = require("./routes/user-routes");
 const recipesRouter = require("./routes/recipes-routes");
 
-const auth = require("./utils/auth/passport");
+//const auth = require("./utils/auth/passport");
 
 app.use(morgan("dev"));
 app.use(helmet());
