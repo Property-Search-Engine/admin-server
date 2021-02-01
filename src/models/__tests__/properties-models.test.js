e3de52bde3a5af75e3fbb44b27a96021f91ed3cb
Víctor Martínez <victorcoder2@gmail.com>
2021-02-01 12:03:08 +0100

    feat: property schema defined (#24)
    feat: property schemas & models defined and tested
    
    fixes #14 #25
    
    Co-authored-by: Damantino <garciadany12@gmail.com>
    Co-authored-by: Mahmoud Mahmoud Abdelfattah <mahmoud.mostafa3107@gmail.com>

---
+++ src/models/__tests__/properties-models.test.js
@@ -0,0 +1,51 @@
+const testServer = require("../../utils/tests/db-test-server");
+const { Property, Home, Office } = require("../properties-model.js");
+const {
+  getHome,
+  getOffice,
+  getTestProperties,
+} = require("../../utils/tests/seedTestDB");
+
+beforeAll(async () => await testServer.initTestServer());
+afterEach(async () => await testServer.clearCollection("properties"));
+afterAll(async () => await testServer.stopTestServer());
+
+describe("properties models", () => {
+  const testHome = getHome();
+  const testOffice = getOffice();
+
+  it("can create a new home model", async () => {
+    const home = await Home.create(testHome);
+
+    expect(home._id).toBeDefined();
+    expect(home.kind).toEqual("Home");
+    expect(home.homeType).toEqual(testHome.homeType);
+    expect(home.sold).toEqual(testHome.sold);
+    expect(home.bedRooms).toEqual(testHome.bedRooms);
+    expect(home.bathRooms).toEqual(testHome.bathRooms);
+    expect(home.description).toEqual(testHome.description);
+    expect(home.price).toEqual(testHome.price);
+    expect(home.condition).toEqual(testHome.condition);
+    expect(home.surface).toEqual(testHome.surface);
+    expect(home.equipment).toEqual(testHome.equipment);
+    expect(home.images).toContainEqual(testHome.images[0]);
+    expect(home.filters).toContainEqual(testHome.filters[0]);
+    expect(home.contactInfo).toBeTruthy();
+    expect(home.address).toBeTruthy();
+  });
+
+  it("can create a new office model", async () => {
+    const office = await Office.create(testOffice);
+
+    expect(office._id).toBeDefined();
+    expect(office.kind).toEqual("Office");
+    expect(office.sold).toEqual(testOffice.sold);
+    expect(office.description).toEqual(testOffice.description);
+    expect(office.price).toEqual(testOffice.price);
+    expect(office.surface).toEqual(testOffice.surface);
+    expect(office.images).toContainEqual(testOffice.images[0]);
+    expect(office.filters).toContainEqual(testOffice.filters[0]);
+    expect(office.contactInfo).toBeTruthy();
+    expect(office.address).toBeTruthy();
+  });
+});
--- src/utils/tests/seedTestDB.js
+++ src/utils/tests/seedTestDB.js
@@ -1,167 +1,102 @@
 const db = require("../../models");
 
-function getTestUsers() {
+const property = {
+  price: 200000,
+  description: "This is the house of your dreams",
+  filters: ["pets_allowed"],
+  images: ["https://geekculture.co/wp-content/uploads/2019/12/Pickle-Rick-3.jpeg"],
+  address: {
+    street: "C/Sant Antoni",
+    number: 50,
+    city: "Cerdañola del Vallés",
+    state: "Catalonia",
+    country: "Spain",
+    coordinates: {
+      lat: 0.1234,
+      long: 1.2314
+    }
+  },
+  contactInfo: {
+    _id: "5d6ede6a0ba62570afcedd3a",
+    phone: "7569283938",
+    email: "patata@mail.com"
+  },
+  sold: false,
+}
+
+function getTestProperties() {
   return [
     {
-      user: {
-        name: "Test User 1 Name",
-        lastname: "Test User 1 Name",
-        email: "testemail1@mail.com",
-        password:
-          "$2b$12$jkAt75qsYkXlypuLK7PEIuj9TIcnZFJbCdwjmGbfbgOBFpvitJysS",
-      },
-      unhashedPassword: "testpassword",
+      kind: "Home",
+      homeType: "house",
+      bedRooms: 3,
+      bathRooms: 2,
+      equipment: "indifferent",
+      condition: "new_home",
+      surface: 200,
+      ...property
     },
     {
-      user: {
-        name: "Test User 2 Name",
-        lastname: "Test User 2 Name",
-        email: "testemail2@mail.com",
-        password:
-          "$2b$12$jkAt75qsYkXlypuLK7PEIuj9TIcnZFJbCdwjmGbfbgOBFpvitJysS",
-      },
-      unhashedPassword: "testpassword",
-    },
-    {
-      user: {
-        name: "Test User 3 Name",
-        lastname: "Test User 3 Name",
-        email: "testemail3@mail.com",
-        password:
-          "$2b$12$jkAt75qsYkXlypuLK7PEIuj9TIcnZFJbCdwjmGbfbgOBFpvitJysS",
-      },
-      unhashedPassword: "testpassword",
-    },
-    {
-      user: {
-        name: "Test User 4 Name",
-        lastname: "Test User 4 Name",
-        email: "testemail4@mail.com",
-        password:
-          "$2b$12$jkAt75qsYkXlypuLK7PEIuj9TIcnZFJbCdwjmGbfbgOBFpvitJysS",
-      },
-      unhashedPassword: "testpassword",
-    },
+      kind: "Office",
+      building_use: "co_working",
+      ...property
+    }
   ];
 }
 
-const RECIPES = [
-  {
-    name: "Test Recipe 1 Title",
-    description: "Test Recipe 1 Description",
-    difficulty: "Fácil",
-    image: "Test Recipe 1 Image 1",
-    serves: 6,
-    ingredients: ["Test Recipe 1 Ingredient 1", "Test Recipe 1 Ingredient 2"],
-    hoursToPrep: 0,
-    minutesToPrep: 40,
-  },
-  {
-    name: "Test Recipe 2 Title",
-    description: "Test Recipe 2 Description",
-    difficulty: "Fácil",
-    image: "Test Recipe 2 Image 1",
-    serves: 4,
-    ingredients: ["Test Recipe 2 Ingredient 1", "Test Recipe 2 Ingredient 2"],
-    hoursToPrep: 0,
-    minutesToPrep: 40,
-  },
-];
+// async function seedTestRecibesDB() {
+//   const testUser = getRecipesRoutesTestUser();
 
-const COMMENTS = [
-  {
-    body:
-      "Nos ha gustado muchísimo esta receta. Sin duda la volveremos a preparar.",
-  },
-  {
-    body:
-      "Es una receta que está bien. Va bien para cuando tienes prisa y no sabes qué cenar.",
-  },
-  {
-    body: "A mis hijos les gusta, a mí no tanto.",
-  },
-  {
-    body: "¡Muy buena!",
-  },
-  {
-    body: "¡Nos ha encantado!",
-  },
-  {
-    body: "Podría ser mejor...",
-  },
-  {
-    body: "Regular",
-  },
-];
+//   const newUser = await db.User.create({
+//     ...testUser.user,
+//     password: testUser.unhashedPassword,
+//   });
 
-function getRandomItem(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+//   const recipesWithAuthor = RECIPES.map((recipe) => ({
+//     ...recipe,
+//     author: newUser._id,
+//   }));
+
+//   const newRecipes = await db.Recipe.insertMany(recipesWithAuthor);
+
+//   const newRecipesIDS = newRecipes.map((recipe) => recipe._id);
+
+//   const newCommentsWithData = COMMENTS.map((comment) => ({
+//     ...comment,
+//     author: newUser._id,
+//     recipe: getRandomItem(newRecipesIDS),
+//   }));
+
+//   const newComments = await db.Comment.insertMany(newCommentsWithData);
+
+//   newComments.forEach(async (comment) => {
+//     await db.Recipe.findOneAndUpdate(
+//       { _id: comment.recipe },
+//       {
+//         $push: {
+//           comments: [comment._id],
+//         },
+//       },
+//     );
+//   });
+// }
+
+// async function getRecipeWithComments() {
+//   return await db.Recipe.findOne({
+//     $where: "this.comments.length > 1",
+//   });
+// }
+
+function getHome() {
+  return getTestProperties()[0];
 }
 
-async function seedTestRecibesDB() {
-  const testUser = getRecipesRoutesTestUser();
-
-  const newUser = await db.User.create({
-    ...testUser.user,
-    password: testUser.unhashedPassword,
-  });
-
-  const recipesWithAuthor = RECIPES.map((recipe) => ({
-    ...recipe,
-    author: newUser._id,
-  }));
-
-  const newRecipes = await db.Recipe.insertMany(recipesWithAuthor);
-
-  const newRecipesIDS = newRecipes.map((recipe) => recipe._id);
-
-  const newCommentsWithData = COMMENTS.map((comment) => ({
-    ...comment,
-    author: newUser._id,
-    recipe: getRandomItem(newRecipesIDS),
-  }));
-
-  const newComments = await db.Comment.insertMany(newCommentsWithData);
-
-  newComments.forEach(async (comment) => {
-    await db.Recipe.findOneAndUpdate(
-      { _id: comment.recipe },
-      {
-        $push: {
-          comments: [comment._id],
-        },
-      },
-    );
-  });
-}
-
-function getUsersRouteTestUser() {
-  return getTestUsers()[0];
-}
-
-function getUserModelTestUser() {
-  return getTestUsers()[1];
-}
-
-function getRecipesRoutesTestUser() {
-  return getTestUsers()[2];
-}
-
-function getAuthenticatedTestUser() {
-  return getRecipesRoutesTestUser();
-}
-
-async function getRecipeWithComments() {
-  return await db.Recipe.findOne({
-    $where: "this.comments.length > 1",
-  });
+function getOffice() {
+  return getTestProperties()[1];
 }
 
 module.exports = {
-  seedTestRecibesDB,
-  getAuthenticatedTestUser,
-  getRecipeWithComments,
-  getUsersRouteTestUser,
-  getUserModelTestUser,
-  getRecipesRoutesTestUser,
+  getTestProperties,
+  getHome,
+  getOffice
 };
