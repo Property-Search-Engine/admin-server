3b09bf1e95f229079a90599e028e58ed35ddcc3c
Víctor Martínez <victorcoder2@gmail.com>
2021-02-03 14:44:54 +0100

    little change

--- src/models/schemas/employee.js
+++ src/models/schemas/employee.js
@@ -39,8 +39,8 @@ const EmployeeSchema = new mongoose.Schema(
     },
     {
         timestamps: true,
-        // toJSON: { virtuals: true },  So `res.json()` and other `JSON.stringify()` functions include virtuals
-        // toObject: { virtuals: true }  So `toObject()` output includes virtuals
+        toJSON: { virtuals: true },  //So `res.json()` and other `JSON.stringify()` functions include virtuals
+        toObject: { virtuals: true }  //So `toObject()` output includes virtuals
     },
 );
 
